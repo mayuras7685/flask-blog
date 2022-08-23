8365960740079d648192706adb30c025e2877edd
mayuras7685 <mayurasodara@gmail.com>
2022-08-23 22:41:20 +0530

    flask run

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from config import Config
+from posts.blueprint import posts
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+app.register_blueprint(posts, url_prefix= '/blog')
+# localhost:5000/blog

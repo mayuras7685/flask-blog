97533d3e156e19335e056d4f298ce1fde648e505
mayuras7685 <mayurasodara@gmail.com>
2022-08-23 22:45:48 +0530

    posts template and blueprints

---
+++ posts/blueprint.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from flask import render_template
+
+
+posts = Blueprint('posts', __name__, template_folder='templates')
+
+@posts.route('/')
+def posts_list():
+  return render_template('posts/posts.html')

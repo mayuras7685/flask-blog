625f2cdb3780f83d82ba1657a7e731bcee1a2256
mayuras7685 <mayurasodara@gmail.com>
2022-08-25 23:35:05 +0530

    slug

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,9 +1,15 @@
 from flask import Blueprint
 from flask import render_template
-
+from models import Post
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 @posts.route('/')
 def posts_list():
-  return render_template('posts/posts.html')
+  posts = Post.query.all()
+  return render_template('posts/posts.html', posts=posts)
+
+@posts.route('/<slug>')
+def post_detail(slug):
+  post = Post.query.filter(Post.slug==slug).first()
+  return render_template('posts/post_detail.html', post=post)

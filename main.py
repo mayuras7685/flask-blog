962c62461b06f94f69be199c88bc9aac0aabbcba
mayuras7685 <mayurasodara@gmail.com>
2022-08-25 23:34:19 +0530

    register add

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from app import app
-import views
+from posts.blueprint import posts
+
+
+app.register_blueprint(posts, url_prefix= '/blog')
+# localhost:5000/blog
+
 
 if __name__ == '__main__':
   app.run()

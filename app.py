3f9b39f7b28010c6748e7ae130f94f63877afe81
mayuras7685 <mayurasodara@gmail.com>
2022-08-27 23:58:03 +0530

    tags routee

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate
+from flask_migrate import MigrateCommand
+from flask_script import Manager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
-
-
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)

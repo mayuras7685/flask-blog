0eed5c81f3cd1af8cc49765882b0b5032d0946ca
mayuras7685 <mayurasodara@gmail.com>
2022-08-27 23:59:25 +0530

    updated

--- models.py
+++ models.py
@@ -10,6 +10,13 @@ def slugify(s):
   pattern = r'[^\w+]'
   return re.sub(pattern, '-', s)
 
+posts_tags = db.Table('posts_tags',
+                      db.Column('post_id', db.Integer,
+                      db.ForeignKey('post.id')),
+                      db.Column('tag_id', db.Integer,
+                      db.ForeignKey('tag.id'))   
+                      )
+
 
 class Post(db.Model):
   id = db.Column(db.Integer, primary_key = True)
@@ -17,7 +24,7 @@ class Post(db.Model):
   slug = db.Column(db.String(140), unique = True)
   body = db.Column(db.Text)
   created = db.Column(db.DateTime, default = datetime.now())
-
+  tags = db.relationship('Tag', secondary = posts_tags, backref=db.backref('posts'), lazy='dynamic')
 
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
@@ -31,3 +38,16 @@ class Post(db.Model):
 
   def __repr__(self):
     return f'<Post id: {self.id}, title:{self.title}>'
+
+
+class Tag(db.Model):
+  id = db.Column(db.Integer, primary_key = True)
+  title = db.Column(db.String(140))
+  slug = db.Column(db.String(140), unique = True)
+  
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.slug = slugify(self.title)
+
+  def __repr__(self):
+    return f'<Tag id: {self.id}, title:{self.title}>'

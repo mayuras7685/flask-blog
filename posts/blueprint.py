e57b5f0522261a2a9129431cc88f1af3941f8362
mayuras7685 <mayurasodara@gmail.com>
2022-08-29 22:24:06 +0530

    create new post route

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,12 +1,44 @@
 from flask import Blueprint
 from flask import render_template
+from flask import request
+
+from flask import redirect
+from flask import url_for
 from models import *
+from posts.forms import PostForm
+from app import db
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
+
+@posts.route('/create', methods=['POST', 'GET'])
+def post_create():
+  form =PostForm()
+
+  if request.method == 'POST':
+    title = request.form.get('title')
+    body = request.form.get('body')
+    
+    try:
+      post = Post(title=title, body=body)
+      db.session.add(post)
+      db.session.commit()
+    except:
+      print('Very long traceback')
+    return redirect(url_for('posts.post_detail', slug=post.slug))
+    
+  return render_template('posts/post_create.html', form=form)
+
+
+
 @posts.route('/')
 def posts_list():
-  posts = Post.query.all()
+  q = request.args.get('q')
+
+  if q:
+    posts = Post.query.filter(Post.title.contains(q)| Post.body.contains(q))
+  else:
+    posts = Post.query.order_by(Post.created.desc())
   return render_template('posts/posts.html', posts=posts)
 
 @posts.route('/<slug>')

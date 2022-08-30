6192960da3349a351f7fe35baa3cf5c56f875064
mayuras7685 <mayurasodara@gmail.com>
2022-08-30 22:52:44 +0530

    secret_key variable

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config:
   DEBUG = True
   SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SECRET_KEY = 'spacexisbestnotablueorigin'

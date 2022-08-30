d74739bebbc089d00bdff1f3dc4f7848c33ecbcf
mayuras7685 <mayurasodara@gmail.com>
2022-08-30 22:52:01 +0530

    admin page

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_migrate import MigrateCommand
 from flask_script import Manager
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +15,15 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
+from models import *
+
+
 migrate = Migrate(app, db)
 manager = Manager(app)
+
 manager.add_command('db', MigrateCommand)
+
+admin = Admin(app)
+
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))

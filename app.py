8619d147b6f3f04845182dc981b6dccd0512a535
mayuras7685 <mayurasodara@gmail.com>
2022-09-02 23:14:43 +0530

    PostAdminView, TagAdminView

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from flask import redirect, url_for, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_migrate import MigrateCommand
 from flask_script import Manager
 from flask_admin import Admin
+from flask_admin import AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from flask_security import SQLAlchemyUserDatastore
 from flask_security import Security
+
+from flask_security import current_user
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -22,11 +27,42 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
-admin = Admin(app)
+class AdminMixin:
+  def is_accessible(self):
+    return current_user.has_role('admin')
 
-admin.add_view(ModelView(Post, db.session))
-admin.add_view(ModelView(Tag, db.session))
+  def inaccessible_callback(self, name, **kwargs):
+    return redirect(url_for('security.login',
+    next=request.url))
+ 
+
+class AdminView(AdminMixin, ModelView):
+  pass
+
+class HomeAdminView(AdminMixin, AdminIndexView):
+  pass
+
+
+class BaseModelView(ModelView):
+  def on_model_change(self, form, model, is_created):
+    if is_created:
+      model.generate_slug()
+    return super().on_model_change(form, model, is_created)
+
+class PostAdminView(AdminMixin, BaseModelView):
+  form_columns = ['title', 'body', 'tags' ]
+
+class TagAdminView(AdminMixin, BaseModelView):
+  pass
+
+
+admin =Admin(app, 'FlaskApp', url='/', index_view=HomeAdminView(name='Home') )
+
+
+
+admin.add_view(PostAdminView(Post, db.session))
+admin.add_view(TagAdminView(Tag, db.session))
 
 # Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-Security = Security(app, user_datastore)
+security = Security(app, user_datastore)

7f36490edeb6be793ccfdf05b237fec9952beb8d
mayuras7685 <mayurasodara@gmail.com>
2022-09-07 21:32:12 +0530

    host='0.0.0.0'

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from app import app
-from app import db
-import views
 from posts.blueprint import posts
 
 
@@ -9,4 +7,4 @@ app.register_blueprint(posts, url_prefix= '/blog')
 
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=False, host='0.0.0.0')
